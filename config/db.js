9941b2d57fefebfbf5c3761c622beba3d6d10e82
yassinebah8 <yassine.bahadda.2@ens.etsmtl.ca>
2026-03-04 09:45:58 -0500

    Fix connexion MySQL Railway via URL

--- config/db.js
+++ config/db.js
@@ -1,11 +1,19 @@
 const mysql = require('mysql2/promise');
-require('dotenv').config();
 
-const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-});
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
 
+let pool;
+
+if (process.env.MYSQL_URL) {
+    pool = mysql.createPool(process.env.MYSQL_URL);
+} else {
+    pool = mysql.createPool({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME
+    });
+}
 module.exports = pool;
